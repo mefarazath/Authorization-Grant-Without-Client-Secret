be774feb6e78541ba5a5185d92543f238b754f38
Farasath Ahamed <farasatha@wso2.com>
2017-10-14 14:36:03 +0530

    Added comments.

--- src/main/java/org/wso2/carbon/identity/oauth2/grant/validator/ExtendedAuthzGrantValidator.java
+++ src/main/java/org/wso2/carbon/identity/oauth2/grant/validator/ExtendedAuthzGrantValidator.java
@@ -2,6 +2,10 @@ package org.wso2.carbon.identity.oauth2.grant.validator;
 
 import org.apache.oltu.oauth2.as.validator.AuthorizationCodeValidator;
 
+/**
+ * Extended Grant Validator for Authorization Grant Type to remove the strict client authentication. This will be
+ * required to enable authorization code grant without client_secret.
+ */
 public class ExtendedAuthzGrantValidator extends AuthorizationCodeValidator {
     public ExtendedAuthzGrantValidator() {
         super();
